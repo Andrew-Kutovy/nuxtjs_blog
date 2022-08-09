e68db5c0a70b31fc4f99e961c9989cc454dff35d
andrew-kutovy <andrijkutovij5@gmail.com>
2022-08-10 02:14:39 +0300

    Initial commit

---
+++ components/Navbar.vue
@@ -0,0 +1,33 @@
+<template>
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark sticky-top">
+    <nuxt-link class="navbar-brand" to="/">
+      <img src="https://upload.wikimedia.org/wikipedia/commons/c/c3/Python-logo-notext.svg" width="30" height="30" alt="logo">
+    </nuxt-link>
+    <nuxt-link class="navbar-brand" to="/">
+      <img src="https://nuxtjs.org/logos/nuxt-square-white.svg" width="30" height="30" alt="logo">
+    </nuxt-link>
+    <button class="navbar-toggler" type="button" data-toggle="collapse"
+            data-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav mr-auto">
+        <li class="nav-item active">
+          <nuxt-link class="nav-link" to="/">Главная</nuxt-link>
+        </li>
+        <li class="nav-item ">
+          <nuxt-link class="nav-link" to="/contact">Контакты</nuxt-link>
+        </li>
+      </ul>
+      <span class="navbar-text mr-2">Username</span>
+      <nuxt-link class="btn btn-outline-light mr-2" to="/signout">Выход</nuxt-link>
+      <nuxt-link class="btn btn-outline-light mr-2" to="/signin">Вход</nuxt-link>
+      <nuxt-link class="btn btn-outline-light mr-2" to="/signup">Регистрация</nuxt-link>
+    </div>
+  </nav>
+</template>
+
+<style scoped>
+
+</style>
